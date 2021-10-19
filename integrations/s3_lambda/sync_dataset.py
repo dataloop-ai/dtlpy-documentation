728a4109be6fcd7e859d22051ae5ae329a596c62
mohamedgalia <mohamed@dataloop.ai>
2021-10-19 15:31:35 +0300

    driver use sdk

--- integrations/s3_lambda/sync_dataset.py
+++ integrations/s3_lambda/sync_dataset.py
@@ -11,16 +11,25 @@ integrations_name = 'S3integration'
 driver_name = 'my_driver_name'
 aws_key = ''
 aws_secret_key = ''
+bucket_name = ''
+region = ''
 
-organization = dl.organizations.get(organization_name='my-org')
+organization = dl.organizations.get(organization_name=organization_name)
 # Create an secret integrations
-organization.add_integrations(name=integrations_name, integrations_type='s3',
-                              options={'key': aws_key, 'secret': aws_secret_key})
-
-
-# Create drivers in the platform using this tutorial. https://dataloop.ai/docs/storage#creating-storage-driver
+integrations = organization.integrations.create(name=integrations_name, integrations_type='s3',
+                                                options={'key': aws_key, 'secret': aws_secret_key})
 
 project = dl.projects.get(project_name=project_name)
+
+project.drivers.create(name=driver_name,
+                       driver_type=dl.ExternalStorage.S3,
+                       integration_id=integrations.id,
+                       bucket_name=bucket_name,
+                       project_id=project.id,
+                       allow_external_delete=True,
+                       region=region,
+                       storage_class="",
+                       path="")
 
 # create a dataset from a driver name, you can also create by the driver ID
 dataset = project.datasets.create(driver=driver_name, dataset_name=dataset_name)
