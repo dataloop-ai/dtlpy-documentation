77efaece937cab885e9fa879c6292c96989c6f8d
Or Shabtay <or@dataloop.ai>
2022-03-10 14:25:27 +0200

    update pipeline example to use sdk

--- examples/pipelines/README.md
+++ examples/pipelines/README.md
@@ -2,31 +2,34 @@
 
 ## Description
 
-This example will include a simple pipeline flow generation from a json template.
+This example will include a simple pipeline flow generation from the SDK and json template.
 
 The example have the following files:
 
-* [Create & Deploy new pipeline ](creating_pipelines.py) - example entry point file (i.e main)
+* [Create & Deploy new pipeline ](create_pipeline.py) - example entry point file (i.e main)
 * [Pipeline template](pipeline_template.json) - the basic template scheme
-* [FaaS definition](pipeline_faas.py) - a simple faas definition
-* [Utility](utils.py) - a short utility (create service, create pipeline)
 
-To run the example, you will need to change the `project_name` to a project you already created in dataloop platform.
-
-`project_name = 'My Project'`
-
-For this example, we will generate a dataset, recipe, package, service and of-course pipeline at your project, with the following names:
+To run the example, you will need to create a project and a dataset and change the `project_name` and `dataset_name` to those you want to use.
 
 ```
-package_name = 'pipeline-faas-example-package'
-dataset_name = 'pipeline-faas-example-dataset'
-pipeline_name = 'pipeline-faas-example'
+project_name = 'My Project'  
+dataset_name = 'My Dataset'
+```
+
+For this example, we will create service (you can use any existing service), and of-course the pipeline in your project.
+If you want to use existing service, set the name in the function, otherwise, you can continue
+
+```
+service_name = 'existing-service-name'
 ```
 
 ## Pipeline
 
-Our pipeline will be consisted of the following components, '*automate*' FaaS - that will be triggerd on any new item created in the dataset, a Task - will be generated upon the first item in the pipline, and an arbitery FaaS - that you can change it's code from the UI.
+Our pipeline will be consisted of the following components:  
+* A 'code-block' FaaS - that you can change its code from the UI.
+* '*automate*' FaaS - that will be triggered on any new item created in the dataset. 
+* A Task - will be generated upon the first item in the pipeline. 
 
-Once an item was created, the pipeline would trigger, the automate function will be called with the item, it will set `item.metadata.system.fromPipe = True`, the item will assigned to your user, enter the task/assignment item, upon pressing 'Done' a complete event will be triggerd, and the item will trigger the arbitery function that will have the 'hello world' code - will set `item.metadata.user = {'Hello': 'World'}` and exit the pipeline.
+Once an item was created, the pipeline would trigger, the code block will be called with the item, it will set `item.metadata['user']['first'] = 'Hello''`, the item will assigned to your user, enter the task/assignment item, upon pressing 'Done' a complete event will be triggered, and the item will continue to the automate function that will add `item.metadata['user']['first'] = 'World''` and exit the pipeline.
 
-![Alt text](../assets/pipeline_example.png)
+![Alt text](../../assets/pipeline_example.png)
---
+++ examples/pipelines/create_pipeline.py
@@ -0,0 +1,90 @@
+import dtlpy as dl
+
+
+def metadata_function_first(item: dl.Item):
+    item.metadata['user']['first'] = 'Hello'
+    item.update(system_metadata=True)
+    return item
+
+
+def metadata_function_second(item: dl.Item):
+    item.metadata['user']['second'] = 'World'
+    item.update(system_metadata=True)
+    return item
+
+
+def create_pipeline(project_name, dataset_name, service_name=None, function_name='run'):
+    project = dl.projects.get(project_name=project_name)
+    dataset = project.datasets.get(dataset_name=dataset_name)
+    print('Initialized params project_name:{}, dataset_name:{}'.format(project.name, dataset.name))
+    #############################################
+    # Publish new package and deploy as service #
+    #############################################
+    if service_name is not None:
+        service = project.services.get(service_name=service_name)
+        print('Using existing service name: {}'.format(service.name))
+    else:
+        service = project.services.deploy(func=metadata_function_second,
+                                          service_name='grayscale-item-service')
+        function_name = 'metadata_function_second'
+        print('Service was created service_name:{}'.format(service.name))
+    ##########################################
+    # Generate pipeline with relevant params #
+    ##########################################
+    recipe = dataset.recipes.list()[0]
+    pipeline = project.pipelines.create(pipeline_name='pipeline-faas-example-dataset')
+
+    code_node = dl.CodeNode(
+        name='My Function',
+        position=(1, 1),
+        project_id=project.id,
+        method=metadata_function_first,
+        project_name=project.name
+    )
+
+    task_node = dl.TaskNode(
+        name='My Task',
+        recipe_id=recipe.id,
+        recipe_title=recipe.title,
+        task_owner='owner',
+        workload=[dl.WorkloadUnit(assignee_id='assignee_id', load=100)],
+        position=(2, 2),
+        project_id=project.id,
+        dataset_id=dataset.id,
+    )
+
+    function_node = dl.FunctionNode(
+        name=service.name,
+        position=(3, 3),
+        service=service,
+        function_name=function_name
+    )
+    pipeline.nodes.add(node=code_node).connect(node=task_node).connect(node=function_node)
+    code_node.add_trigger()
+    pipeline.update()
+
+    print('Pipeline was created pipeline_name:{}'.format(pipeline.name))
+    ###########################
+    # Installing the pipeline #
+    ###########################
+    print('Installing pipeline')
+    pipeline.install()
+    print('Opening pipeline in console...')
+    pipeline.open_in_web()
+
+    ###############
+    # Cleaning up #
+    ###############
+    # dataset.delete()
+    # service.delete()
+    # pipeline.delete()
+
+
+if __name__ == "__main__":
+    dl.setenv('rc')
+    project_name = 'My Project pipeline example'
+    dataset_name = 'pipeline-faas-example-dataset'
+    service_name = None  # or existing service name in the project
+    create_pipeline(project_name=project_name,
+                    service_name=service_name,
+                    dataset_name=dataset_name)
