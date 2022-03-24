13998a0f0d23f76e7be223dc1e525fc9b9900f8c
Shira Navot <shira@dataloop.ai>
2022-03-24 12:35:34 +0000

    yolo script - in progress

--- model_management/object_detection/yolov5_training.py
+++ model_management/object_detection/yolov5_training.py
@@ -1,3 +1,5 @@
+
+from posixpath import dirname
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
@@ -6,97 +8,142 @@ import os
 import sys
 import inspect
 
-sys.path.insert(0, '/home/shira/workspace/distillator-obsolete/dtlpy') #HACK: local hack, remove
+sys.path.insert(0, '/home/shira/workspace/distillator-obsolete/dtlpy')  # HACK: local hack, remove
 sys.path.insert(1, '/home/shira/workspace')
 
-
-import dtlpy as dl
-from dtlpy.ml import train_utils
-
-dl.setenv('prod')
 from yolov5 import model_adapter as yol
-
-project = dl.projects.get('distillator')
-
-
-# model = yol.model_creation(project_name=project.name)
-model = dl.models.get('yolo-v5')
-
-
-# snapshot = yol.snapshot_creation(project_name=project.name, model=model, yolo_ver='yolov5s', env='prod')
-snapshot = model.snapshots.get('pretrained-yolov5s')
-
-# model.snapshots.list().to_df()
-
-dataset = project.datasets.get(None, '61680bfa82e9a80fc61262e9') # gtsrb
-# dataset.to_df()
+from dtlpy.ml import train_utils
+import dtlpy as dl
 
 
 
-adapter = model.build()
-adapter.load_from_snapshot(snapshot=snapshot) # local_path=snapshot.bucket.local_path
+
+def get_globals():
+    model = dl.models.get(model_name='yolo-v5')
+    snapshot = model.snapshots.get('pretrained-yolo-v5-small')
+    model.snapshots.list().to_df()
+    return model, snapshot
 
 
-item = dl.items.get(item_id='6205097d8ea6ad0abe7b90ba') #dog
-image = np.asarray(Image.open(item.download()))
+def create_sample_dataset(project):
+    try:
+        dataset = project.datasets.get(dataset_name='FruitImage')
+    except dl.exceptions.NotFound:
+        dataset = project.datasets.create('FruitImage')
+        _ = dataset.items.upload(local_path='../../assets/sample_datasets/FruitImage/items/train/*',
+                                 local_annotations_path='../../assets/sample_datasets/FruitImage/json/train',
+                                 remote_path='train')
+        _ = dataset.items.upload(local_path='../../assets/sample_datasets/FruitImage/items/test/*',
+                                 local_annotations_path='../../assets/sample_datasets/FruitImage/json/test',
+                                 remote_path='test')
+        dataset.labels = ['apple', 'banana', 'orange', 'mixed']
 
-# use the adapter to inference without uploading annotations
-all_annotations = adapter.predict_items([item], with_upload=False, min_score=0.1)
+    dataset.to_df()
 
-item_annotations = all_annotations[0]  # predict items returns a list for each of the input items
-print(item_annotations.print(to_return=True))
-# print(item.annotations.list().print(to_return=True))
-print(item_annotations.to_df())
-
-# add the items to the image
-annotated_image = item.annotations.show(image=image, thickness=5)
-# plt.imshow(annotated_image)
-
-
-#train on new dataset
-
-# # split the dataset to 2 partitions - 80% train 20% validation - randomly
-partitions = {dl.SnapshotPartitionType.TRAIN: 0.8,
-              dl.SnapshotPartitionType.VALIDATION: 0.2}
-
-# # use DQL to set the two directories and the train/val split
-# partitions = {dl.SnapshotPartitionType.TRAIN: dl.Filters(field='dir', values='/train'),
-#               dl.SnapshotPartitionType.VALIDATION: dl.Filters(field='dir', values='/test')}
-
-cloned_dataset = train_utils.prepare_dataset(dataset,
-                                             filters=None,
-                                             partitions=partitions)
+    return dataset
 
 
-snapshot_name='test-yolov5'
-try:
-    new_snapshot = model.snapshots.get(snapshot_name=snapshot_name)
-except Exception:
-    bucket = project.buckets.create(bucket_type=dl.BucketType.ITEM, model_name=model.name, snapshot_name=snapshot_name)
-    new_snapshot = snapshot.clone(snapshot_name=snapshot_name,
-                                dataset_id=cloned_dataset.id,
-                                bucket=bucket,
-                                configuration={'batch_size': 2,
-                                                'start_epoch': 0,
-                                                'max_epoch': 5,
-                                                'id_to_label_map': {(v-1):k for k, v in dataset.instance_map.items()}
-                                                })
+def create_model_and_snapshot(project):
+    model = yol.model_creation(project_name=project.name)
+    snapshot = yol.snapshot_creation(model, yolo_size='small')
+
+    return model, snapshot
+
+def pretrained_inference(item_id, model, snapshot):
+    ###
+    project = dl.projects.get('distillator')
+    dataset = project.datasets.get(dataset_name='FruitImage')
+    snapshot.configuration['id_to_label_map'] = {}
+    ###
+
+    adapter = model.build()  # local_path='/home/shira/workspace/pytorch_adapters', from_local=True
+    adapter.load_from_snapshot(snapshot=snapshot)  # local_path=snapshot.bucket.local_path
+    item = dl.items.get(item_id=item_id)
+    image = np.asarray(Image.open(item.download()))
+
+    # use the adapter to inference without uploading annotations
+    all_annotations = adapter.predict_items([item], with_upload=False, min_score=0.1)
+
+    item_annotations = all_annotations[0]  # predict items returns a list for each of the input items
+    print(item_annotations.to_df())
+
+    # add the items to the image
+    annotated_image = item.annotations.show(image=image, thickness=5)
+    plt.imshow(annotated_image)
 
 
-adapter.load_from_snapshot(snapshot=new_snapshot)
-root_path, data_path, output_path = adapter.prepare_training()
+def train_on_new_dataset(model, project, snapshot, dataset):
 
-print("Training {!r} with snapshot {!r} on data {!r}".format(model.name, new_snapshot.id, data_path))
-adapter.train(data_path=data_path, output_path=output_path)
+    partitions = {dl.SnapshotPartitionType.TRAIN: dl.Filters(field='dir', values='/train'),
+                  dl.SnapshotPartitionType.VALIDATION: dl.Filters(field='dir', values='/test')}
 
-adapter.save_to_snapshot(local_path=output_path, replace=True)
+    snapshot_name = f'trained-{dataset.name}'
+    try:
+        new_snapshot = model.snapshots.get(snapshot_name=snapshot_name)
+    except dl.exceptions.NotFound:
+        cloned_dataset = train_utils.prepare_dataset(dataset,
+                                                     filters=None,
+                                                     partitions=partitions)
+        bucket = project.buckets.create(bucket_type=dl.BucketType.ITEM, model_name=model.name, snapshot_name=snapshot_name)
+        new_snapshot = snapshot.clone(snapshot_name=snapshot_name,
+                                      dataset_id=cloned_dataset.id,
+                                      bucket=bucket)
+
+    new_snapshot.configuration.update({'batch_size': 16,
+                                       'start_epoch': 0,
+                                       'max_epoch': 5,
+                                       })
+
+    adapter = model.build()
+    adapter.load_from_snapshot(snapshot=new_snapshot,
+                               configuration=new_snapshot.configuration)
+
+    root_path, data_path, output_path = adapter.prepare_training(root_path=os.path.join('tmp', new_snapshot.id))
+
+    print("Training {!r} with snapshot {!r} on data {!r}".format(model.name, new_snapshot.id, data_path))
+    adapter.train(data_path=data_path,
+                  output_path=output_path)
+
+    adapter.snapshot.bucket.list_content().to_df()
+
+    adapter.save_to_snapshot(local_path=output_path,
+                             replace=True)
+
+    adapter.snapshot.bucket.list_content().to_df()
 
 
-# Predict New Snapshot on Local Item
-adapter.load_from_snapshot(snapshot=new_snapshot)
+def main(args, **kwargs):
+    project = dl.projects.get(args.project)
+    model, snapshot = get_globals()
 
-item = dl.items.get(item_id='')
-image = Image.open(item.download())
+    if args.mode == 'train':
+        if not args.dataset:
+            dataset = create_sample_dataset(project)
+        else:
+            dataset = project.datasets.get(None, args.dataset)
+        dataset.to_df()
+        train_on_new_dataset(model=model,
+                             project=project,
+                             snapshot=snapshot,
+                             dataset=dataset)
+    elif args.mode == 'inference':
+        pretrained_inference(item_id=args.item,
+                             model=model,
+                             snapshot=snapshot)
 
-all_annotations = adapter.predict_items([item], with_upload=True)  # use with_upload = False
-plt.imshow(item.annotations.show(np.asarray(image), thickness=5))
+
+if __name__ == '__main__':
+    import argparse
+    dl.setenv('prod')
+    parser = argparse.ArgumentParser(description='local runner for model management testing')
+
+    parser.add_argument('--env', '-e', default='prod', help='dtlpy env')
+    parser.add_argument('--project', '-p', default='distillator', help='dtlpy project name',)  # required=True) #TODO: remove comment
+    parser.add_argument('--dataset', '-d', default='', help='dtlpy dataset id')
+    parser.add_argument('--item', '-i', default='6205097d8ea6ad0abe7b90ba', help='dtlpy single item id')
+    parser.add_argument('--mode', '-m', default='inference', help='inference or training')
+
+    args = parser.parse_args()
+    # args.mode = 'train'  # TODO: remove
+    main(args)
+    print('Done!')
