68d73d95086528248ecb8742c7b1a04b0fb97e78
mohamedgalia <mohamed@dataloop.ai>
2022-07-04 20:02:21 +0300

    DAT-27003 Create upstream sync for GCP

---
+++ examples/integrations/gcs_cloud_function/delete_function.py
@@ -0,0 +1,20 @@
+import dtlpy as dl
+
+DATASET_ID = ''
+DTLPY_USERNAME = ''
+DTLPY_PASSWORD = ''
+
+
+def create_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    dl.login_m2m(email=DTLPY_USERNAME, password=DTLPY_PASSWORD)
+    dataset = dl.datasets.get(dataset_id=DATASET_ID,
+                              fetch=False  # to avoid GET the dataset each time
+                              )
+    file_name = file['name']
+    dataset.items.delete(filename=file_name)
