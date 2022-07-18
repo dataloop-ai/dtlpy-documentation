1939b78f915373331559bba26fcb54a44234f762
ShlomiAvigdor <87410965+ShlomiAvigdor@users.noreply.github.com>
2022-07-18 17:47:45 +0300

    Update mds.py

--- docs_build/tutorials_templates/task_workflows/tasks_and_assignments/create_a_task/mds.py
+++ docs_build/tutorials_templates/task_workflows/tasks_and_assignments/create_a_task/mds.py
@@ -1,20 +1,18 @@
 def func1():
     """
     ## Create a Task
-    To reach the tasks and assignments repositories go to <a href="https://sdk-docs.dataloop.ai/en/latest/repositories.html#module-dtlpy.repositories.tasks" target="_blank">tasks</a> and <a href="https://sdk-docs.dataloop.ai/en/latest/repositories.html#module-dtlpy.repositories.assignments" target="_blank">assignments</a>.
+
+    This tutorial explains how to create a workflow annotation task. A task is created on items, and can optionally provide custom statuses, assignees and workload.
+    When a task is created with assignees, assignments are created accordingly.
     
-      
-    To reach the tasks and assignments entities go to <a href="https://sdk-docs.dataloop.ai/en/latest/entities.html#module-dtlpy.entities.task" target="_blank">tasks</a> and <a href="https://sdk-docs.dataloop.ai/en/latest/entities.html#module-dtlpy.entities.assignment" target="_blank">assignments</a>.
-    ### Creating a Task with Assignments
-    There are a couple of ways to create a task with assignments.
-    #### 1. By Folder Directory
+    ### 1. By Folder Directory
     This example will create a task for items that match a filter. The items will be divided equally between annotator's assignments:
     """
 
 
 def func2():
     """
-    #### 2. By Filters
+    ### 2. By Filters
     This example will create a task for items that match a filter. The items will be divided equally between the annotator's assignments:
 
     <div style="background-color: lightblue; color: black; width: 50%; padding: 10px; border-radius: 15px 5px 5px 5px;"><b>Note</b><br>
@@ -25,34 +23,34 @@ def func2():
 
 def func3():
     """
-    #### 3. List of Items
+    ### 3. List of Items
     Create a task from a list of items. The items will be divided equally between annotator's assignments:
     """
 
 
 def func4():
     """
-    #### 4. Full Dataset
+    ### 4. Entire Dataset
     Create a task from all of the items in the dataset. The items will be divided equally between annotator's assignments:
     """
 
 
 def func5():
     """
-    ### Add items to an existing task
+    ## Add items to an existing task
     Adding items to an existing task will create new assignments (for new assignee/s).
 
-    #### 1. By Filters
+    ### 1. By Filters
     """
 
 
 def func6():
     """
-    #### 2. Single Item
+    ### 2. Single Item
     """
 
 
 def func7():
     """
-    #### 3. List of Items
+    ### 3. List of Items
     """
