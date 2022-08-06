36983d479eb2e09bbea77571e4551c2ad6a5a298
ShlomiAvigdor <87410965+ShlomiAvigdor@users.noreply.github.com>
2022-08-06 14:39:20 +0300

    minor-text-updates-6-8

--- docs_build/tutorials_templates/annotations_image/classification_point_and_pose/mds.py
+++ docs_build/tutorials_templates/annotations_image/classification_point_and_pose/mds.py
@@ -24,4 +24,6 @@ def section3():
 def section4():
     """
     # Pose Annotation
+    Pose annotations are a collection of points that follows a certain template, for example a 'skeleton' for tracking key-point on people showing in image or video items.
+    Templates are created in the Dataloop platform, at the instructions settings of a recipe.
     """
