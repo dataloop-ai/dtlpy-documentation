de942207598cf2c11e12f3c0b68622a6d87cd2b1
Or Shabtay <or@dataloop.ai>
2022-10-11 15:32:55 +0300

    model mgmt snippets

--- examples/model_mgmt/snippets/deploy_model.py
+++ examples/model_mgmt/snippets/deploy_model.py
@@ -1,10 +1,8 @@
 import dtlpy as dl
 
 package = dl.packages.get(package_name='resnet')
-pretrained_model = package.models.get(model_name='pretrained-resnet')
+model = package.models.get(model_name='pretrained-resnet')
 
-model = pretrained_model.clone(model_name='my pretrained',
-                               description='pretrained cloned in to my project',
-                               project_id="projectId")
+dataset = dl.datasets.get(dataset_id='datasetId')
 
-model.deploy()
+model.evaluate(dataset=dataset)
