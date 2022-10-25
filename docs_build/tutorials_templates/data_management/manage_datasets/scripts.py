a58e294aecd556f31d3fc0c1bd305acaf3d11787
OfirDataloopAI <ofir,g@dataloop.ai>
2022-10-25 20:01:20 +0300

    updated scripts file

--- docs_build/tutorials_templates/data_management/manage_datasets/scripts.py
+++ docs_build/tutorials_templates/data_management/manage_datasets/scripts.py
@@ -1,33 +1,70 @@
-def section1():
-    dataset = project.datasets.create(dataset_name='my-dataset-name')
+import dtlpy as dl
 
 
-def section2():
-    project = dl.projects.get(project_name='my-project-name')
+def section1(project, dataset_name):
+    """
+    Required parameters:
+
+    :param dl.Project project:
+    :param str dataset_name: 'my-dataset-name'
+    """
+
+    dataset = project.datasets.create(dataset_name=dataset_name)
+
+
+def section2(project_name, driver, dataset_name):
+    """
+    Required parameters:
+
+    :param str project_name: 'my-project-name'
+    :param str driver: 'my-dataset-name'
+    :param str dataset_name: 'dataset_name'
+    """
+
+    project = dl.projects.get(project_name=project_name)
     # Get your drivers list
     project.drivers.list().print()
     # Create a dataset from a driver name. You can also create by the driver ID.
-    dataset = project.datasets.create(driver='my_driver_name', dataset_name="my_dataset_name")
+    dataset = project.datasets.create(driver=driver, dataset_name=dataset_name)
 
 
-def section3():
+def section3(project, dataset_id):
+    """
+    Required parameters:
+
+    :param dl.Project project:
+    :param str dataset_id: 'my-dataset-id'
+    """
+
     datasets = project.datasets.list()
-    dataset = project.datasets.get(dataset_id='my-dataset-id')
+    dataset = project.datasets.get(dataset_id=dataset_id)
 
 
-def section4():
-    dataset.items.make_dir(directory="/directory/name")
+def section4(dataset, directory):
+    """
+    Required parameters:
+
+    :param dl.Dataset dataset:
+    :param str directory: "/directory/name"
+    """
+
+    dataset.items.make_dir(directory=directory)
 
 
-def section5():
+def section5(source_folder, destination_folder, source_project_name, source_dataset_name, destination_project_name, destination_dataset_name):
+    """
+    Required parameters:
+
+    :param str source_folder: '/source_folder'
+    :param str destination_folder: '/destination_folder'
+    :param str source_project_name: 'source_project_name'
+    :param str source_dataset_name: 'source_dataset_name'
+    :param str destination_project_name: 'destination_project_name'
+    :param str destination_dataset_name: 'destination_dataset_name'
+    """
+
     copy_annotations = True
     flat_copy = False  # if true, it copies all dir files and sub dir files to the destination folder without sub directories
-    source_folder = '/source_folder'
-    destination_folder = '/destination_folder'
-    source_project_name = 'source_project_name'
-    source_dataset_name = 'source_dataset_name'
-    destination_project_name = 'destination_project_name'
-    destination_dataset_name = 'destination_dataset_name'
     # Get source project dataset
     project = dl.projects.get(project_name=source_project_name)
     dataset_from = project.datasets.get(dataset_name=source_dataset_name)
