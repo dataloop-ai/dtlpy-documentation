f0f7fcc425261a51855b47e5da5ee7a8ee3448f9
Or Shabtay <or@dataloop.ai>
2022-12-22 09:02:47 +0200

    with referencing

--- docs_build/glossary/glossary.md
+++ docs_build/glossary/glossary.md
@@ -3,94 +3,94 @@
 [A](#a) | [B](#b) | [C](#c) | [D](#d) | [E](#e) | [F](#f) | [G](#g) | [H](#h) | [I](#i) | [J](#j) | [K](#k) | [L](#l) | [M](#m) | [N](#n) | [O](#o) | [P](#p) | [Q](#q) | [R](#r) | [S](#s) | [T](#t) | [U](#u) | [V](#v) | [W](#w) | [X](#x) | [Y](#y) | [Z](#z)
 
 ## A
-### Annotation ($Entity)
+### <a name="annotation"></a>Annotation ($Entity)
 A JSON format representing the objects and labels exists in an [Item](#item).
-### Artifacts ([Entity](#entity))
+### <a name="artifacts"></a>Artifacts ([Entity](#entity))
 Large files (binaries) that we don't want to pack inside a package (which should only contain code), but are still needed during the deployment of a package.They are uploaded separately and downloaded when the service is initiated.    Usually need to be added to the .gitignoreThere are three types: Item, Local, or Link
-### Assignment ([Entity](#entity))
+### <a name="assignment"></a>Assignment ([Entity](#entity))
 A single annotators’ items for manual review and/or annotation. Items can be redistributed or reassigned between assignments.The annotator is referred to as an “assignee”
-### Attribute ([Entity](#entity))
+### <a name="attribute"></a>Attribute ([Entity](#entity))
 
 ## B
-### Binaries (Item)
+### <a name="binaries"></a>Binaries ([Item](#item))
 The content of any type of file (image, video, pdf, etc)
-### Box ([Entity](#entity))
+### <a name="box"></a>Box ([Entity](#entity))
 A bounding box [Annotation](#annotation) type. Represented by 2 point for top-left and bottom-right. type=`box`
 ## C
-### Clone
+### <a name="clone"></a>Clone
 A shallow copy of the item (with or without metadata and annotations) without copying the binaries.
-### Codebase ([Entity](#entity))
+### <a name="codebase"></a>Codebase ([Entity](#entity))
 -four types of code bases: item codebase vs git codebase vs local codebase vs filesystem (currently no one uses this, and is likely for when working in a remote container)-limited to 100MB
-### Command
+### <a name="command"></a>Command
 
 ## D
-### Dataset ([Entity](#entity))
+### <a name="dataset"></a>Dataset ([Entity](#entity))
 
-### DQL - Dataloop Query Language
+### <a name="dql---dataloop-query-language"></a>DQL - Dataloop Query Language
 
 ## E
-### Entity
+### <a name="entity"></a>Entity
 A Dataloop data model object. Represented by a json.
-### Execution ([Entity](#entity))
+### <a name="execution"></a>Execution ([Entity](#entity))
 
 ## F
-### Function ([Entity](#entity))
+### <a name="function"></a>Function ([Entity](#entity))
 definitions of where to find the class and how to load itFaaS is the function
 ## G
 ## H
 ## I
-### Instance Catalog
+### <a name="instance-catalog"></a>Instance Catalog
 
 ### <a name="item"></a>Item ([Entity](#entity))
 An object representing some file and metadata
 ## J
 ## K
 ## L
-### Label ([Entity](#entity))
+### <a name="label"></a>Label ([Entity](#entity))
 A piece of text that gives information about an annotation instance. Contains also the color, display name and children of the anntoation.
-### LogSample
+### <a name="logsample"></a>LogSample
 
 ## M
-### Metadata
+### <a name="metadata"></a>Metadata
 A dictionary object that contains metadata of the Dl object. Usually includes “system” and “user” in the metadata fields to distinguish between necessary and custom user metadata.
-### Model
+### <a name="model"></a>Model
 The representation of what a machine learning algorithm has learned from the training dataIs a python (pytorch, tensorflow) representation of a NN architecture with or without loaded weights (pth, pb, hdf5 files)
-### Model ([Entity](#entity))
+### <a name="model"></a>Model ([Entity](#entity))
 A Dataloop Model is a combination of data (dl.Dataset), configurations (json dictionary), and code (dl.Package) to represent a learnable instance of the data.You can train, evaluate, compare, and deploy a dl.Model.Out-of-the-box models are available on the platform with the scope attribute “public”, as opposed to project specific models with “project” scope.
-### Model Adapter
+### <a name="model-adapter"></a>Model Adapter
 A python class to wrap a generic ML code (train, predict etc) for standardizing models (and frameworks) to match the requirements of the Dataloop API.Log Sample, Metric Sample, A single data point is used to measure and compare different models and metrics. e.g. saving the train/validation loss and accuracy of a training sessionOffline ModeFor model management, only dl.Model is used to save configurations and training/evaluation metrics, while code and weights and biases are kept locally.Gives the ability to manage training runs, view live metrics, and compare dl.Models (configurations and metrics)Online ModeAllows integration into the Dataloop platform (after creating Model Adapter).Gives the ability to create custom buttons (e.g. in the annotations studio), train, deploy and evaluate using the Model Management Pages, using models inside a pipeline etc.)
-### Model Weights / Model Artifacts
+### <a name="model-weights-/-model-artifacts"></a>Model Weights / Model Artifacts
 Those are the files that are saved/created by training a machine learning algorithm on a dataset. They are adjusted using optimization algorithms and unique to each training session.
-### Module ([Entity](#entity))
+### <a name="module"></a>Module ([Entity](#entity))
 A json representing the python class and functions (location, IOs etc.)
 ## N
-### Neural Network
+### <a name="neural-network"></a>Neural Network
 A machine learning model that is composed of layers consisting of simple connected units or neurons followed by nonlinearities.
 ## O
 ## P
-### Package ([Entity](#entity))
+### <a name="package"></a>Package ([Entity](#entity))
 A package is the bundle of code and definitions that can be used for creating models or deploying services. Code is a dl.Codebase entity, and definitions include modules, functions, IOs, and the code entry point.Can be Python, nodeJS (maybe everything, we don't care).Used to deploy a service and create an executable version of that code.Limited to 100MBCan be public/global, or specific to a project
-### Pipeline ([Entity](#entity))
+### <a name="pipeline"></a>Pipeline ([Entity](#entity))
 A collection of functions (machine processing) and tasks (human processing) that creates a processing flow. Structured from nodes (processing units) and connections (to move data types between nodes)
-### Pipeline Cycle
+### <a name="pipeline-cycle"></a>Pipeline Cycle
 A single run over the pipeline. From the first node to the last available node to run
-### Pod type
+### <a name="pod-type"></a>Pod type
 
-### Polygon ([Entity](#entity))
+### <a name="polygon"></a>Polygon ([Entity](#entity))
 A polygon [Annotation](#annotation) type. Represented by a list of (x,y) points. type=`segment`
 ## Q
 ## R
-### Repository
+### <a name="repository"></a>Repository
 
 ## S
-### Segmentation ([Entity](#entity))
+### <a name="segmentation"></a>Segmentation ([Entity](#entity))
 A binary mask [Annotation](#annotation) type. Represented by mask with the same size as the image, 0 for background, 1 for the object. type=`binary`
-### Service ([Entity](#entity))
+### <a name="service"></a>Service ([Entity](#entity))
 A deployed [Package](#package) that serves the code. Given the matching input to a function, it will run it and return the output, e.g. if we have code in our [Package](#package) for converting RGB images to grayscale, the dl.Service would run the code and upload the grayed image.
 ## T
-### Task ([Entity](#entity))
+### <a name="task"></a>Task ([Entity](#entity))
 A set of items to be reviewed/annotated by human workers. A task is divided into assignments.Can be managed by a queue (Pulling Task) or by workload (percentage of items per assignee).The task is connected to a recipe for instructions and world definitions (ontology)
-### Trigger ([Entity](#entity))
+### <a name="trigger"></a>Trigger ([Entity](#entity))
 
 ## U
 ## V
--- docs_build/glossary/main.py
+++ docs_build/glossary/main.py
@@ -2,73 +2,73 @@ import string
 import re
 
 definitions = [
-    {"name": "Entity",
+    {"header": "Entity",
      "description": "A Dataloop data model object. Represented by a json."},
-    {"name": "Repository",
+    {"header": "Repository",
      "description": ""},
-    {"name": "Item ($[Entity])",
+    {"header": "Item ($[Entity])",
      "description": "An object representing some file and metadata"},
-    {"name": "Binaries (Item)",
+    {"header": "Binaries ($[Item])",
      "description": "The content of any type of file (image, video, pdf, etc)"},
-    {"name": "Clone",
+    {"header": "Clone",
      "description": "A shallow copy of the item (with or without metadata and annotations) without copying the binaries."},
-    {"name": "Annotation ($Entity)",
+    {"header": "Annotation ($Entity)",
      "description": "A JSON format representing the objects and labels exists in an $[Item]."},
-    {"name": "Label ($[Entity])",
+    {"header": "Label ($[Entity])",
      "description": "A piece of text that gives information about an annotation instance. Contains also the color, display name and children of the anntoation."},
-    {"name": "Attribute ($[Entity])",
+    {"header": "Attribute ($[Entity])",
      "description": ""},
-    {"name": "Box ($[Entity])",
+    {"header": "Box ($[Entity])",
      "description": "A bounding box $[Annotation] type. Represented by 2 point for top-left and bottom-right. type=`box`"},
-    {"name": "Polygon ($[Entity])",
+    {"header": "Polygon ($[Entity])",
      "description": "A polygon $[Annotation] type. Represented by a list of (x,y) points. type=`segment`"},
 
-    {"name": "Segmentation ($[Entity])",
+    {"header": "Segmentation ($[Entity])",
      "description": "A binary mask $[Annotation] type. Represented by mask with the same size as the image, 0 for background, 1 for the object. type=`binary`"},
 
-    {"name": "Task ($[Entity])",
+    {"header": "Task ($[Entity])",
      "description": "A set of items to be reviewed/annotated by human workers. A task is divided into assignments."
                     "Can be managed by a queue (Pulling Task) or by workload (percentage of items per assignee)."
                     "The task is connected to a recipe for instructions and world definitions (ontology)"},
-    {"name": "Assignment ($[Entity])",
+    {"header": "Assignment ($[Entity])",
      "description": "A single annotators’ items for manual review and/or annotation. Items can be redistributed or reassigned between assignments."
                     "The annotator is referred to as an “assignee”"},
-    {"name": "Package ($[Entity])",
+    {"header": "Package ($[Entity])",
      "description": "A package is the bundle of code and definitions that can be used for creating models or deploying services. Code is a dl.Codebase entity, and definitions include modules, functions, IOs, and the code entry point."
                     "Can be Python, nodeJS (maybe everything, we don't care)."
                     "Used to deploy a service and create an executable version of that code."
                     "Limited to 100MB"
                     "Can be public/global, or specific to a project"},
-    {"name": "Module ($[Entity])",
+    {"header": "Module ($[Entity])",
      "description": "A json representing the python class and functions (location, IOs etc.)"},
-    {"name": "Function ($[Entity])",
+    {"header": "Function ($[Entity])",
      "description": "definitions of where to find the class and how to load it"
                     "FaaS is the function"},
-    {"name": "Codebase ($[Entity])",
+    {"header": "Codebase ($[Entity])",
      "description": "-four types of code bases: item codebase vs git codebase vs local codebase vs filesystem (currently no one uses this, and is likely for when working in a remote container)"
                     "-limited to 100MB"},
-    {"name": "Artifacts ($[Entity])",
+    {"header": "Artifacts ($[Entity])",
      "description": "Large files (binaries) that we don't want to pack inside a package (which should only contain code), but are still needed during the deployment of a package."
                     "They are uploaded separately and downloaded when the service is initiated.    "
                     "Usually need to be added to the .gitignore"
                     "There are three types: Item, Local, or Link"},
-    {"name": "Service ($[Entity])",
+    {"header": "Service ($[Entity])",
      "description": "A deployed $[Package] that serves the code. Given the matching input to a function, it will run it and return the output, e.g. if we have code in our $[Package] for converting RGB images to grayscale, the dl.Service would run the code and upload the grayed image."},
-    {"name": "Execution ($[Entity])",
+    {"header": "Execution ($[Entity])",
      "description": ""},
-    {"name": "Trigger ($[Entity])",
+    {"header": "Trigger ($[Entity])",
      "description": ""},
-    {"name": "Pod type",
+    {"header": "Pod type",
      "description": ""},
-    {"name": "Instance Catalog",
+    {"header": "Instance Catalog",
      "description": ""},
-    {"name": "Model ($[Entity])",
+    {"header": "Model ($[Entity])",
      "description": "A Dataloop Model is a combination of data (dl.Dataset), configurations (json dictionary), and code (dl.Package) to represent a learnable instance of the data."
                     "You can train, evaluate, compare, and deploy a dl.Model."
                     "Out-of-the-box models are available on the platform with the scope attribute “public”, as opposed to project specific models with “project” scope."},
-    {"name": "LogSample",
+    {"header": "LogSample",
      "description": ""},
-    {"name": "Model Adapter",
+    {"header": "Model Adapter",
      "description": "A python class to wrap a generic ML code (train, predict etc) for standardizing models (and frameworks) to match the requirements of the Dataloop API."
                     "Log Sample, Metric Sample, A single data point is used to measure and compare different models and metrics. e.g. saving the train/validation loss and accuracy of a training session"
                     "Offline Mode"
@@ -77,27 +77,27 @@ definitions = [
                     "Online Mode"
                     "Allows integration into the Dataloop platform (after creating Model Adapter)."
                     "Gives the ability to create custom buttons (e.g. in the annotations studio), train, deploy and evaluate using the Model Management Pages, using models inside a pipeline etc.)"},
-    {"name": "Model",
+    {"header": "Model",
      "description": "The representation of what a machine learning algorithm has learned from the training data"
                     "Is a python (pytorch, tensorflow) representation of a NN architecture with or without loaded weights (pth, pb, hdf5 files)"},
-    {"name": "Model Weights / Model Artifacts",
+    {"header": "Model Weights / Model Artifacts",
      "description": "Those are the files that are saved/created by training a machine learning algorithm on a dataset. They are adjusted using optimization algorithms and unique to each training session."},
-    {"name": "Neural Network",
+    {"header": "Neural Network",
      "description": "A machine learning model that is composed of layers consisting of simple connected units or neurons followed by nonlinearities."},
-    {"name": "Command",
+    {"header": "Command",
      "description": ""},
-    {"name": "Pipeline ($[Entity])",
+    {"header": "Pipeline ($[Entity])",
      "description": "A collection of functions (machine processing) and tasks (human processing) that creates a processing flow. Structured from nodes (processing units) and connections (to move data types between nodes)"},
-    {"name": "Pipeline Cycle",
+    {"header": "Pipeline Cycle",
      "description": "A single run over the pipeline. From the first node to the last available node to run"},
 
-    {"name": "Metadata",
+    {"header": "Metadata",
      "description": "A dictionary object that contains metadata of the Dl object. Usually includes “system” and “user” in the metadata fields to distinguish between necessary and custom user metadata."},
-    {"name": "DQL - Dataloop Query Language",
+    {"header": "DQL - Dataloop Query Language",
      "description": ""},
-    {"name": "Dataset ($[Entity])",
+    {"header": "Dataset ($[Entity])",
      "description": ""},
-    {"name": "",
+    {"header": "",
      "description": ""},
 
 ]
@@ -111,27 +111,40 @@ def create():
     md_string += " | ".join(headers)
     md_string += "\n"
     md_string += "\n"
-    sorted_definitions = sorted(definitions, key=lambda d: d['name'].lower())
+    sorted_definitions = sorted(definitions, key=lambda d: d['header'].lower())
     for letter in letters:
         letter_definitions = [definition for definition in sorted_definitions if
-                              definition["name"].lower().startswith(letter.lower())]
+                              definition["header"].lower().startswith(letter.lower())]
         md_string += f"## {letter.upper()}\n"
         for definition in letter_definitions:
-            name = definition['name']
+            header = definition['header']
             description = definition['description']
-            # name = "this is a $[test] with two $[dollars]"
-            grps = re.finditer(r"\$\[([A-Za-z0-9_]+)\]", name)
+
+            ###
+            # replace $ with the current link
+            # header = "this is a $[test] with two $[dollars]"
+            grps = re.finditer(r"\$\[([A-Za-z0-9_]+)\]", header)
             for grp in grps:
                 txt = grp.group(1)
-                name = name.replace(grp.group(), f'[{txt}](#{txt.lower()})')
+                header = header.replace(grp.group(), f'[{txt}](#{txt.lower()})')
 
             grps = re.finditer(r"\$\[([A-Za-z0-9_]+)\]", description)
             for grp in grps:
                 txt = grp.group(1)
                 description = description.replace(grp.group(), f'[{txt}](#{txt.lower()})')
 
-            # replace $ with the current link
-            md_string += f"### {name}\n"
+            ###
+            # set header name for the linking
+            if header.find('(') == -1:
+                tag_name = header
+            else:
+                tag_name = header[:(header.find('('))]
+            tag_name = tag_name.strip().lower().replace(' ', '-')
+            header = f'<a name="{tag_name}"></a>{header}'
+
+            ###
+            # add to the md strings
+            md_string += f"### {header}\n"
             md_string += f"{description}\n"
 
     with open('docs_build/glossary/glossary.md', 'w', encoding='utf8') as f:
