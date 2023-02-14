6a8018ecc28f52b14b758c2c9ffa564d9aed9949
mohamedgalia <mohamed@dataloop.ai>
2023-02-14 19:19:19 +0200

    add suppot to aws path

--- docs_build/tutorials_templates/data_management/cloud_storage/binding_with_aws/scripts.py
+++ docs_build/tutorials_templates/data_management/cloud_storage/binding_with_aws/scripts.py
@@ -15,18 +15,26 @@ def section1():
         dataset = dl.datasets.get(dataset_id=DATASET_ID,
                                   fetch=False  # to avoid GET the dataset each time
                                   )
-
+        driver_path = dl.drivers.get(driver_id=dataset.driver).path
         for record in event['Records']:
             # Get the bucket name
             bucket = record['s3']['bucket']['name']
 
             # Get the file name
             filename = urllib.parse.unquote_plus(record['s3']['object']['key'], encoding='utf-8')
-
+            remote_path = None
+            if driver_path == '/':
+                driver_path = None
+            if driver_path is not None and driver_path not in filename:
+                return
+            if driver_path:
+                remote_path = filename.replace(driver_path, '')
+            else:
+                remote_path = filename
             if 'ObjectRemoved' in record['eventName']:
                 # On delete event - delete the item from Dataloop
                 try:
-                    dtlpy_filename = '/' + filename
+                    dtlpy_filename = '/' + remote_path
                     filters = dl.Filters(field='filename', values=dtlpy_filename)
                     dataset.items.delete(filters=filters)
                 except Exception as e:
@@ -38,6 +46,6 @@ def section1():
                     # upload the file
                     path = 'external://' + filename
                     # dataset.items.upload(local_path=path, overwrite=True) # if overwrite is required
-                    dataset.items.upload(local_path=path)
+                    dataset.items.upload(local_path=path, remote_path=remote_path)
                 except Exception as e:
                     raise e
