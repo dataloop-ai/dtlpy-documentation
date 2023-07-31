4980fd7802fb633d8273b7fd29bcdacc45802039
Or Shabtay <or@dataloop.ai>
2023-07-31 13:56:16 +0300

    fixed broken links check

--- docs_build/check_links.py
+++ docs_build/check_links.py
@@ -4,9 +4,9 @@ import re
 
 
 def check_links():
-    p = Path('docs_build').glob('**/mds.py')
+    p = Path('./tutorials').glob('**/*.md')
     files = [x for x in p if x.is_file()]
-
+    print(f'Found {len(files)} md files to check... ')
     for file in files:
         print(f'Checking {file}')
         with open(file, 'r', encoding='UTF-8') as f:
@@ -15,10 +15,21 @@ def check_links():
         res = re.findall(r'(https?://[a-zA-Z0-9./\-_#]+)', string)
         if res is not None:
             for url in res:
-                resp = requests.get(url)
-                if not resp.ok:
-                    print(f'    BROKEN: {url}, in {file}')
-                    # assert False
+                if 'local.dataloop.ai' in url:
+                    continue
+                try:
+                    resp = requests.get(url, timeout=3)
+                    if not resp.ok:
+                        print(f'    BROKEN: {url}, in {file}')
+                        # assert False
+                except requests.exceptions.HTTPError:
+                    print(f'    BROKEN: {url}, in {file}: HTTPError')
+                except requests.exceptions.ConnectionError as errc:
+                    print(f'    BROKEN: {url}, in {file}: ConnectionError')
+                except requests.exceptions.Timeout:
+                    print(f'    BROKEN: {url}, in {file}: Timeout')
+                except requests.exceptions.RequestException:
+                    print(f'    BROKEN: {url}, in {file}: RequestException')
 
 
 if __name__ == "__main__":
