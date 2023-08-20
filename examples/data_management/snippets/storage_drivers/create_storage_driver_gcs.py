7d618ceae2e9b1649edf960ba8d7fc45481b26fa
Yonatan Mesika <yonatan.m@dataloop.ai>
2023-08-20 13:36:48 +0300

    Add 'Data Management' code snippet examples

---
+++ examples/data_management/snippets/storage_drivers/create_storage_driver_gcs.py
@@ -0,0 +1,9 @@
+import dtlpy as dl
+
+project = dl.projects.get('project_name')
+driver = project.drivers.create(name='driver_name',
+                                driver_type=dl.ExternalStorage.GCS,
+                                integration_id='integration_id',
+                                bucket_name='bucket_name',
+                                allow_external_delete=True,
+                                path='')
