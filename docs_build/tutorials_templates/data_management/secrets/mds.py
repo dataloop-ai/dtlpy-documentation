64e58852cb4efb3ce180f0b83325d491d64fdd17
itais <itai.s@dataloop.ai>
2023-10-31 13:28:36 +0200

    add secrets to tutorials

---
+++ docs_build/tutorials_templates/data_management/secrets/mds.py
@@ -0,0 +1,10 @@
+def section1():
+    """
+    # Secrets
+
+    If you already have your data managed and organized on a cloud storage service, such as S3, you may want to
+    utilize that with Dataloop, and not upload the binaries and create duplicates.
+
+
+    ### Create A key value secret
+    """
