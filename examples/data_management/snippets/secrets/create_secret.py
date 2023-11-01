78758d859fde64f43c72a4f2d2af9c234ce148e0
itais <itai.s@dataloop.ai>
2023-11-01 11:15:35 +0200

    add secrets to tutorials

--- examples/data_management/snippets/secrets/create_secret.py
+++ examples/data_management/snippets/secrets/create_secret.py
@@ -2,5 +2,5 @@ import dtlpy as dl
 
 organization = dl.organizations.get(organization_name='my_org')
 organization.integrations.create(name='my_secret',
-                                 integrations_type=dl.ExternalStorage.KEY_VALUE,
+                                 integrations_type=dl.IntegrationType.KEY_VALUE,
                                  options={'key': 'my_key', 'value': 'my_value'})
