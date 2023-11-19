04a391243da53d83eb8d3e1993dc8ac82e4b7142
Or Shabtay <or@dataloop.ai>
2023-11-19 12:08:56 +0200

    mgmt snippets

--- examples/model_mgmt/snippets/clone_model.py
+++ examples/model_mgmt/snippets/clone_model.py
@@ -1,8 +1,28 @@
 import dtlpy as dl
 
-package = dl.packages.get(package_name='resnet')
-pretrained_model = package.models.get(model_name='pretrained-resnet50')
+##############################################
+# Clone a pre-trained model without finetune #
+##############################################
 
+global_package = dl.packages.get(package_name='resnet')
+pretrained_model = global_package.models.get(model_name='pretrained-resnet50')
+
+my_project = dl.projects.get(project_id="$PROJECT_ID")
 model = pretrained_model.clone(model_name='my pretrained',
-                               description='pretrained cloned in to my project',
-                               project_id="$PROJECT_ID")
+                               description='cloned pretrained in my project',
+                               project_id=my_project.id)
+
+###############################
+# Clone and fine-tune a model #
+###############################
+dataset = my_project.datasets.get(dataset_name='Finetuning')
+
+train_filter = dl.Filters('dir', '/train')
+val_filter = dl.Filters('dir', '/validation')
+
+model = pretrained_model.clone(model_name='finetuned-resnet',
+                               description='cloned for finetune in my project',
+                               project_id=my_project.id,
+                               dataset=dataset,
+                               train_filter=train_filter,
+                               validation_filter=val_filter)
