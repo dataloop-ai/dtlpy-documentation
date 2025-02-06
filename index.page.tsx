1a5202a289e4d626e7022b6d1483281b6751fcf6
Or Shabtay <or@dataloop.ai>
2025-02-06 14:09:59 +0200

    cards

--- index.page.tsx
+++ index.page.tsx
@@ -1,18 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
 import tutorialsIcon from './assets/site/icons/tutorials.svg';
 import onboardingIcon from './assets/site/icons/onboarding.svg';
 import resourcesIcon from './assets/site/icons/resources.svg';
-import {
-  LandingLayout,
-  Button,
-  Box,
-  FlexSection,
-  Flex,
-  Jumbotron,
-  WideTile,
-  H1,
-  H2,
-  NavBar,
-} from '@redocly/developer-portal/ui';
+import { ArrowRightIcon, Button } from '@redocly/theme';
+import { CardWithCode } from './@theme/components/CardWithCode/CardWithCode';
+import { Card } from '@redocly/theme/markdoc/components/Cards/Card';
+import { Cards } from '@redocly/theme/markdoc/components/Cards/Cards';
+
+const Container = styled.div`
+  padding: 2rem;
+`;
+
+const Header = styled.div`
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: normal;
+  margin-bottom: 2rem;
+`;
 
 interface HomePageProps {
   location: {
@@ -24,31 +38,26 @@ interface HomePageProps {
 
 export default function HomePage({ location }: HomePageProps) {
   return (
-    <LandingLayout>
-      <Jumbotron>
-        <NavBar location={location} standalone={false} />
-        <H1>Dataloop Developers Portal</H1>
-        <H2>A developer portal for beginners and advanced users</H2>
-        <Flex p={20} justifyContent="center">
-          <Button variant="outlined" size="xlarge" to="tutorials/getting_started/sdk_overview/chapter.md">
-            Get started
-          </Button>
-        </Flex>
-      </Jumbotron>
-      <Box my={25}>
-        <FlexSection justifyContent="space-around" flexWrap="wrap">
-          <WideTile to="tutorials/tutorials.mdx" icon={tutorialsIcon} header="Tutorials">
-            Step-by-step guides to get started with Dataloop
-          </WideTile>
-          <WideTile to="onboarding/onboarding.md" icon={onboardingIcon} header="Onboarding">
-            Get up and running with Dataloop platform
-          </WideTile>
-          <WideTile to="resources/resources.mdx" icon={resourcesIcon} header="Resources">
-            SDKs, APIs and developer tools
-          </WideTile>
-        </FlexSection>
-      </Box>
-    </LandingLayout>
+    <Container>
+      <Header>
+        <Title>Dataloop Developers Portal</Title>
+        <Subtitle>A developer portal for beginners and advanced users</Subtitle>
+        <Button variant="outlined" size="xlarge" to="tutorials/getting_started/sdk_overview/chapter.md">
+          Get started <ArrowRightIcon />
+        </Button>
+      </Header>
+      <Cards>
+        <Card title="Tutorials" to="tutorials/tutorials.mdx" icon={tutorialsIcon}>
+          Step-by-step guides to get started with Dataloop
+        </Card>
+        <Card title="Onboarding" to="onboarding/onboarding.md" icon={onboardingIcon}>
+          Get up and running with Dataloop platform
+        </Card>
+        <Card title="Resources" to="resources/resources.mdx" icon={resourcesIcon}>
+          SDKs, APIs and developer tools
+        </Card>
+      </Cards>
+    </Container>
   );
 }
 
